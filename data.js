7757d1b4b1f314a3e16028fdb552404bf0d55630
Paresh Patel <paresh.patel@crestinfosystems.com>
2023-02-07 17:22:37 +0530

    CSV Export

---
+++ data.js
@@ -0,0 +1,12 @@
+import { Parser } from "@json2csv/plainjs";
+import axios from "axios";
+import fs from "fs";
+
+try {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  const parser = new Parser();
+  const csv = parser.parse(res.data);
+  fs.writeFileSync("output.csv", csv);
+} catch (err) {
+  console.error(err);
+}
